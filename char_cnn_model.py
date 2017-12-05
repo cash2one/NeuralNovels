d7a1a68f0c91ed7dd68e3c98b7f68549e6ac2fe8
Matthew Dangerfield <matthew.dangerfield@yandex.com>
2017-12-05 10:27:32 -0600

    fix generation with all G.A. henty data

--- char_cnn_model.py
+++ char_cnn_model.py
@@ -1,102 +1,95 @@
 from __future__ import print_function
 from keras.models import Sequential, load_model
-from keras.layers import Dense, Activation, LSTM, BatchNormalization, Dropout
+from keras.layers import Dense, Activation, LSTM, BatchNormalization, Dropout, TimeDistributed, Conv1D, MaxPooling1D, Flatten
 from keras.optimizers import RMSprop
 from keras.utils import to_categorical
-from sklearn.utils import shuffle
 import numpy as np
 from progressbar import ProgressBar
-import os, random, sys, argparse, operator, gc
+import random, sys, argparse, operator, gc
 from time import sleep
 
 from book_utils import *
 from embedding_utils import *
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' # filter out INFO
-
-model_file_name = 'checkpoints/1024_512_batchnorm.h5'
-
 file_names = get_file_names_written_by('George Alfred Henty')
 text = ''.join(get_files_contents(file_names))
-print('text length', len(text))
 
-print('Tokenizing...')
-words, word_index, idx_to_word = tokenize_words(text, 20000)
+max_word_len = 50
+words, char_index, idx_to_char = tokenize_words_to_chars(text, max_word_length=max_word_len)
 
 # TODO: probably have a more random way of doing the train/val split
-#val_split = 0.05
-#train_words = words[:int((1 - val_split) * len(words))]
-#val_words = words[int(val_split * len(words)):]
+val_split = 0.05
+train_words = words[:int((1 - val_split) * len(words))]
+val_words = words[int(val_split * len(words)):]
 
 maxlen = 30
 step = 3
-batch_size = 1200
+batch_size = 512
 # Derived using formula for length of range at stackoverflow.com/questions/31839032
-#total_train_samples = (len(train_words) - maxlen - 1) // step + 1
-#total_val_samples = (len(val_words) - maxlen - 1) // step + 1
-total_samples = (len(words) - maxlen - 1) // step + 1 
+total_train_samples = (len(train_words) - maxlen - 1) // step + 1
+total_val_samples = (len(val_words) - maxlen - 1) // step + 1
 def get_chunk(data):
     # cut the text in semi-redundant sequences of maxlen words
+    words_idx = 0
+
     # I derived this using the formula for the length of a range and wolfram alpha. It should work
     words_per_batch = (batch_size - 1) * step + maxlen + 1
-    words_order = list(range(len(data) // words_per_batch - 1))
-
     while True:
-        random.shuffle(words_order)
-        for words_idx in words_order:
-            words_batch = data[words_idx * words_per_batch : (words_idx + 1) * words_per_batch]
-            sentences = []
-            next_words = []
+        if (words_idx + 1) * words_per_batch >= len(data): words_idx = 0
+        words_batch = data[words_idx * words_per_batch : (words_idx + 1) * words_per_batch]
+        sentences = []
+        next_chars = []
 
-            for i in range(0, words_per_batch - maxlen, step):
-                sentences.append(words_batch[i: i + maxlen])
-                next_words.append(words_batch[i + maxlen])
+        for i in range(0, words_per_batch - maxlen, step):
+            sentences.append(words_batch[i: i + maxlen])
+            next_chars.append(words_batch[i + maxlen])
+        import pdb; pdb.set_trace()
 
-            x = np.array(sentences)
-            y = to_categorical(next_words, num_classes=len(word_index))
+        x = np.array([to_categorical(sentence, num_classes=len(char_index)) for sentence in sentences])
+        y = to_categorical(next_chars, num_classes=len(char_index))
 
-            yield shuffle(x, y)
+        yield (x, y)
+
+        words_idx += 1
 
 def build_model(load_weights):
+    # build the model
     print('Build model...')
     if load_weights:
-        return load_model(model_file_name)
+        return load_model('gru_char_rnn.h5')
     else:
         model = Sequential([
-                    get_embedding_layer(word_index, maxlen, trainable=True),
-                    BatchNormalization(),
-                    LSTM(1024, dropout=0.5, return_sequences=True),
-                    BatchNormalization(),
-                    LSTM(512, dropout=0.5),
-                    BatchNormalization(),
-                    Dense(len(word_index), activation='softmax')
+                    TimeDistributed(Conv1D(300, 5, activation='relu'), input_shape=(maxlen, max_word_len, len(char_index))),
+                    TimeDistributed(MaxPooling1D()),
+                    TimeDistributed(Flatten()),
+                    LSTM(1024, return_sequences=True),
+                    LSTM(1024),
+                    Dense(len(char_index), activation='softmax')
             ])
         model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['acc'])
         model.summary()
         return model
 
-def train(n_iter, n_words, beam_width, load_checkpoint):
-    model = build_model(load_checkpoint)
+def train(n_iter, n_chars, beam_width):
+    # train the model, output generated text after each iteration
+    model = build_model(False)
     for iteration in range(1, n_iter + 1):
         print()
         print('-' * 50)
         print('Iteration', iteration)
-        model.fit_generator(get_chunk(words),
-                            #(total_train_samples // batch_size) // 4,
-                            total_samples // batch_size,
+        model.fit_generator(get_chunk(train_words),
+                            total_train_samples // batch_size,
                             epochs=1,
-                            #validation_data=get_chunk(val_words),
-                            #validation_steps=total_val_samples // batch_size
-                            )
+                            validation_data=get_chunk(val_words),
+                            validation_steps=total_val_samples // batch_size)
 
-        print('Saving model...')
-        model.save(model_file_name)
-
-        for diversity in [1.4, 1.6, 1.8, 2.0]:
+        for diversity in [1.2, 1.4, 1.6, 1.8]:
             print()
-            beam_search(model, n_words, beam_width, diversity, sys.stdout)
+            beam_search(model, n_chars, beam_width, diversity, sys.stdout)
             print()
-
+    
+    print('Saving model...')
+    model.save('gru_char_rnn.h5')
     return model
 
 def scale_prediction(prediction, temperature):
@@ -113,7 +106,7 @@ def beam_search(model, n_words, beam_width, diversity, stream):
     print('----- diversity:', diversity)
     start_index = random.randint(0, len(words) - maxlen - 1)
     init_sentence = np.array(words[start_index: start_index + maxlen])
-    init_sentence = np.concatenate((init_sentence, np.zeros(n_words))).astype(np.int)
+    init_sentence = np.concatenate((init_sentence, np.zeros(n_words)))
 
     beams = [{ 'sentence': init_sentence, 'proba': 1.0 }]
 
@@ -123,7 +116,7 @@ def beam_search(model, n_words, beam_width, diversity, stream):
         new_beams = []
 
         x_pred = np.array([beam['sentence'][i:i + maxlen] for beam in beams])
-        all_preds = model.predict(x_pred, verbose=0, batch_size=batch_size)
+        all_preds = model.predict(x_pred, verbose=0, batch_size=256)
         for preds, beam in zip(all_preds, beams):
             beam['preds'] = preds
 
@@ -131,12 +124,7 @@ def beam_search(model, n_words, beam_width, diversity, stream):
             sentence = beam['sentence']
             preds = scale_prediction(beam['preds'], diversity)
 
-            best_idxs = np.random.choice(
-                            len(preds),
-                            size=min(np.count_nonzero(preds > 0), beam_width),
-                            replace=False,
-                            p=preds
-                        )
+            best_idxs = np.random.choice(len(preds), size=beam_width, replace=False, p=preds)
 
             for pred_idx in best_idxs:
                 new_sentence = np.copy(sentence)
@@ -151,22 +139,20 @@ def beam_search(model, n_words, beam_width, diversity, stream):
         sum_probs = sum([beam['proba'] for beam in beams])
         for beam in beams: beam['proba'] /= sum_probs # avoid going to zero
 
-    stream.write(detokenize(beams[0]['sentence'], idx_to_word))
+    stream.write(detokenize(beams[0]['sentence'], idx_to_char))
 
-parser = argparse.ArgumentParser(description='train/generate text with word rnn')
+parser = argparse.ArgumentParser(description='train/generate text with char rnn')
 parser.add_argument('--mode', type=str, default='train', help='Either "train" or "generate"')
-parser.add_argument('--iter', type=int, default=3, help='Number of training iterations')
+parser.add_argument('--iter', type=int, default=10, help='Number of training iterations')
 parser.add_argument('--words', type=int, default=80, help='Number of words for  beam search')
 parser.add_argument('--diversity', type=float, default=1.6, help='Temperature for beam search')
 parser.add_argument('--beam_width', type=int, default=30, help='Beam width for beam search')
-parser.add_argument('--load_checkpoint', dest='load_checkpoint', action='store_true', help='Load the model from a checkpoint file')
-parser.set_defaults(load_checkpoint=False)
 
 FLAGS = parser.parse_args()
 
 if __name__ == '__main__':
     if FLAGS.mode == 'train':
-        trained_model = train(FLAGS.iter, FLAGS.words, FLAGS.beam_width, FLAGS.load_checkpoint)
+        trained_model = train(FLAGS.iter, FLAGS.words, FLAGS.beam_width)
         beam_search(trained_model, 1000, FLAGS.beam_width, FLAGS.diversity, open('./generated_words.md', 'w'))
     elif FLAGS.mode == 'generate':
         beam_search(build_model(True), FLAGS.words, FLAGS.beam_width, FLAGS.diversity, open('./generated_words.md', 'w'))
--- embedding_utils.py
+++ embedding_utils.py
@@ -3,12 +3,12 @@ from collections import Counter
 
 import numpy as np
 from keras.layers import Embedding
-from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
 def tokenize_words(text, vocab_size):
     open('/tmp/in.txt', 'w').write(text)
     os.system('~/Code/dl/datasets/stanford-parser-full-2017-06-09/tokenize.sh')
-    tokens = open('/tmp/out.txt').read().split()
+    tokens = open('/tmp/out.txt').read().replace('\n', ' \n ').split(' ')
     print('Total tokens in dataset', len(tokens))
 
     top_tokens = [tok for (tok, cnt) in Counter(tokens).most_common(vocab_size - 1)] # -1 for UNK
@@ -21,13 +21,28 @@ def tokenize_words(text, vocab_size):
     idx_to_word = {v: k for k, v in word_index.items()} 
     return (sequences, word_index, idx_to_word)
 
+def tokenize_words_to_chars(text, max_word_length=50):
+    open('/tmp/in.txt', 'w').write(text)
+    os.system('~/Code/dl/datasets/stanford-parser-full-2017-06-09/tokenize.sh')
+    tokens = open('/tmp/out.txt').read().split()
+    print('Total tokens in dataset', len(tokens))
+
+    char_index = {tok: idx for (idx, tok) in enumerate(set(text))}
+    print('Found {} unique chars.'.format(len(char_index)))
+
+    sequences = [[char_index[char] for char in word] for word in tokens]
+    sequences = pad_sequences(sequences, value=len(char_index), maxlen=max_word_length, padding='post', truncating='post')
+
+    idx_to_char = {v: k for k, v in char_index.items()} 
+    return (sequences, char_index, idx_to_char)
+
 def detokenize(words, idx_to_word):
     word_list = [idx_to_word[idx] for idx in words]
     open('/tmp/in.txt', 'w').write(' '.join(word_list))
     os.system('~/Code/dl/datasets/stanford-parser-full-2017-06-09/detokenize.sh')
     return open('/tmp/out.txt').read()
 
-def get_embedding_layer(word_index, input_length):
+def get_embedding_layer(word_index, input_length, trainable=False):
     EMBEDDING_DIM = 300
     embeddings_index = {}
     f = open(os.path.expanduser('~/Code/dl/datasets/glove.42B.300d.txt'))
@@ -40,7 +55,7 @@ def get_embedding_layer(word_index, input_length):
     
     print('Found %s word vectors.' % len(embeddings_index))
     
-    embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
+    embedding_matrix = np.random.normal(size=(len(word_index) + 1, EMBEDDING_DIM))
     for word, i in word_index.items():
         embedding_vector = embeddings_index.get(word.lower())
         if embedding_vector is not None:
@@ -51,7 +66,7 @@ def get_embedding_layer(word_index, input_length):
                                 EMBEDDING_DIM,
                                 weights=[embedding_matrix],
                                 input_length=input_length,
-                                trainable=False)
+                                trainable=trainable)
 
     return embedding_layer
 
